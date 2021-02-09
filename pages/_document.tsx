808d1befa2770db5a4d109e3cf0a53b8a9a27077
Yunseop Kim <c.o.d.e@kakao.com>
2021-02-09 22:45:16 +0900

    viewport 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,20 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
+      <Html lang="en">
+        <Head>
+          <title>Tweeter</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
 
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }

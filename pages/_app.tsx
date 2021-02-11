ae5cc60d77bcd36db07e093c61c8aac921d00da9
Yunseop Kim <c.o.d.e@kakao.com>
2021-02-11 15:33:06 +0900

    viewport warning 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import Head from "next/head";
+import React from "react";
 
-import '../styles/globals.css'
-import '../styles/reset.css'
+import "../styles/globals.css";
+import "../styles/reset.css";
 
 function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,11 @@
-import * as React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <title>Tweeter</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
+        <Head />
 
         <body>
           <Main />
